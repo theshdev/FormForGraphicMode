caa6b1c63d9c16cc82ac4c40010e0118488bf604
theshdev <30322746+theshdev@users.noreply.github.com>
2017-07-21 11:32:47 -0300

    Add files via upload

--- src/formforgraphicmode.sh
+++ src/formforgraphicmode.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 zenity --version || exit
-if [[ $1 == "-h" ]];then
+if [ "$1" == "-h" ]; then
 	echo "-u uninstall the program"; exit
-elif [[ $1 == "-u" ]];then
+elif [ "$1" == "-u" ]; then
 	rm -rf /usr/bin/formforgraphicmode; exit
+else
+	echo "Running FFGM"
 fi
 
 
 path=$(zenity --entry --title="FFGM" --text="Enter the file path: ")
-if [[ $path == $(echo) ]];then
+if [[ "$path" == "$(echo)" ]];then
 	exit
 fi
 nper=$(zenity --entry --title="FFGM" --text="How many questions do you want to generate? ")
-if [[ $nper == $(echo) ]];then
+if [ "$nper" == "$(echo)" ];then
 	exit
 fi
 
@@ -32,8 +34,15 @@ var=1
 while true; do
 	varp="pergunta$var"
 	resposta=$(zenity --entry --title="FFGM" --text="${!varp}")
-	if [[ $resposta == $(echo) ]];then
-		break
+	if [[ ${!varp} != $(echo) ]];then
+		if [[ $resposta == $(echo) ]];then
+			break
+		fi
+	fi
+	if [[ ${!varp} == $(echo) ]];then
+		if [[ $resposta == $(echo) ]];then
+			echo "Sorry for the bug, please report in https://github.com/theshdev/FormForGraphicMode/issues"
+		fi
 	fi
 	echo ${!varp} $resposta >> $path
 	var=$(($var+1))
